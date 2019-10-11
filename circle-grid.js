5efd7b22c2f1a537560771901326ab1567f1f0a1
Betty <55335473+ZhongweiDeng@users.noreply.github.com>
2019-10-11 01:18:30 -0700

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  
+  for (var x = 20; x < width; x += 20) {
+    for (var y = 20; y < height; y += 20) {
+      fill(255)
+      ellipse(x, y, 15)
+    }
+  }
+}
